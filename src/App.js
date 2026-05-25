57cdf5fb25eae61b0b36c9bb969387bf5a68731b
Raj <211345463+RAJHANSH-QU@users.noreply.github.com>
2026-05-25 14:50:41 +0530

    fix routing for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Builder from './pages/Builder';
 import '@fontsource/playfair-display';
@@ -7,12 +7,12 @@ import '@fontsource/inter';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/builder" element={<Builder />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
